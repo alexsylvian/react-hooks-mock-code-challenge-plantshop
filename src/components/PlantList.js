879669086c57a4fe6c29a4c6443d4b3a565e4810
alexsylvian <lazeratus1@gmail.com>
2023-12-17 16:52:27 -0500

    page startup finished

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((res) => res.json())
+    .then((plantData) => setPlants(plantData))
+  }, [])
+
+  const listOfPlants = plants.map(plant =>(
+    <PlantCard key={plant.id} name={plant.name} price={plant.price} image={plant.image} />
+  ))
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{listOfPlants}</ul>
   );
 }
 
